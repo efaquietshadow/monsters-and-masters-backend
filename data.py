48fad9a28929b22e83404c535b42640f44aa59b0
kaylynfletcher <efaquietshadow@gmail.com>
2020-06-24 19:34:26 -0600

    fixes cursed errors and added basic skills

--- data.py
+++ data.py
@@ -2,59 +2,216 @@ import random
 
 data = {
     "races": {
-        "centaur": {
-            "name": "Centaur"
-        },
-        "cursed": {
-            "name": "Cursed"
-        },
         "dragonkin": {
-            "name": "Dragonkin"
+            "name": "Dragonkin",
+            "base-health": 30,
+              "base-skills": {
+                "str": 2,
+                "con": 1,
+                "spe": 2,
+                "ste": 0,
+                "dex": 1,
+                "int": 1,
+                "cha": 0
+            }
         },
         "dwarf": {
-            "name": "Dwarf"
+            "name": "Dwarf",
+            "base-health": 34,
+            "base-skills": {
+                "str": 3,
+                "con": 3,
+                "spe": 0,
+                "ste": 0,
+                "dex": 0,
+                "int": 0,
+                "cha": 1
+            }
         },
         "elemental": {
-            "name": "Elemental"
+            "name": "Elemental",
+            "base-health": 27,
+            "sub-race": ["water", "earth", "air", "fire", "dark", "light"],
+            "base-skills": {
+                "str": 1,
+                "con": 1,
+                "spe": 1,
+                "ste": 1,
+                "dex": 1,
+                "int": 1,
+                "cha": 1
+            }
         },
         "elf": {
-            "name": "Elf"
+            "name": "Elf",
+            "base-health": 27,
+            "base-skills": {
+                "str": 1,
+                "con": 1,
+                "spe": 1,
+                "ste": 0,
+                "dex": 1,
+                "int": 1,
+                "cha": 2
+            }
         },
         "fae": {
-            "name": "Elf"
+            "name": "Fae",
+            "base-health": 21,
+            "base-skills": {
+                "str": -2,
+                "con": 0,
+                "spe": 3,
+                "ste": 0,
+                "dex": 1,
+                "int": 1,
+                "cha": 3
+            }
         },
         "giant": {
-            "name": "Giant"
+            "name": "Giant",
+            "base-health": 42,
+            "base-skills": {
+                "str": 5,
+                "con": 3,
+                "spe": 0,
+                "ste": 0,
+                "dex": -1,
+                "int": -1,
+                "cha": 1
+            }
         },
         "goblin": {
-            "name": "Goblin"
+            "name": "Goblin",
+            "base-health": 21,
+            "base-skills": {
+                "str": 0,
+                "con": 0,
+                "spe": 2,
+                "ste": 1,
+                "dex": 2,
+                "int": 1,
+                "cha": 1
+            }
         },
-        "hafling": {
-            "name": "Hafling"
+        "half-ling": {
+            "name": "Half-ling",
+            "base-health": 25,
+            "base-skills": {
+                "str": 0,
+                "con": 1,
+                "spe": 1,
+                "ste": 2,
+                "dex": 2,
+                "int": 1,
+                "cha": 0
+            }
         },
         "human": {
-            "name": "Human"
+            "name": "Human",
+            "base-health": 30,
+            # make randomized base skills == 7
+            "base-skills": {
+                "str": 1,
+                "con": 1,
+                "spe": 1,
+                "ste": 1,
+                "dex": 1,
+                "int": 1,
+                "cha": 1
+            }
         },
         "ignisaka": {
-            "name": "Ignisaka"
+            "name": "Ignisaka",
+            "base-health": 30,
+            "base-skills": {
+                "str": 2,
+                "con": 1,
+                "spe": 1,
+                "ste": 0,
+                "dex": 2,
+                "int": 0,
+                "cha": 1
+            }
         },
         "oliforon": {
-            "name": "Oliforon"
+            "name": "Oliforon",
+            "base-health": 30,
+            # add can modify with base animal
+            "base-skills": {
+                "str": 2,
+                "con": 1,
+                "spe": 2,
+                "ste": 1,
+                "dex": 1,
+                "int": 0,
+                "cha": 0
+            }
         },
         "shapeshifter": {
-            "name": "Shapeshifter"
+            "name": "Shapeshifter",
+            "base-health": 26,
+            "base-skills": {
+                "str": 0,
+                "con": 1,
+                "spe": 0,
+                "ste": 3,
+                "dex": 1,
+                "int": 1,
+                "cha": 1
+            }
         },
         "siren": {
-            "name": "Siren"
+            "name": "Siren",
+            "base-health": 28,
+            "base-skills": {
+                "str": 0,
+                "con": 1,
+                "spe": 1,
+                "ste": 1,
+                "dex": 1,
+                "int": 1,
+                "cha": 2
+            }
         },
         "taur": {
-            "name": "Taur"
+            "name": "Taur",
+            "base-health": 29,
+            "base-skills": {
+                "str": 2,
+                "con": 1,
+                "spe": 1,
+                "ste": 0,
+                "dex": 1,
+                "int": 1,
+                "cha": 1
+            }
         },
         "vampire": {
-            "name": "Vampire"
+            "name": "Vampire",
+            "base-health": 32,
+            "base-skills": {
+                "str": [1, 2],
+                "con": 1,
+                "spe": [1, 2],
+                "ste": 2,
+                "dex": 1,
+                "int": 0,
+                "cha": 0
+            }
         },
         "zombie": {
-            "name": "Zombie"
+            "name": "Zombie",
+            "base-health": 32,
+            "base-skills": {
+                "str": 1,
+                "con": 4,
+                "spe": 0,
+                "ste": 0,
+                "dex": 1,
+                "int": -1,
+                "cha": -2
+            }
         }
     }
 }
--- main.py
+++ main.py
@@ -6,10 +6,15 @@ class_list = ['Bard', 'Mage', 'Merchant', 'Paladin', 'Ranger', 'Rogue', 'Warrior
 
 random_race = choose_race()
 race_name = random_race['name']
-if race_name == 'Cursed':
-  race_name = race_name + " " + random.choice(random_race)
+race_health = random_race['base-health']
+base_skills = random_race["base-skills"]
+
+is_cursed = random.randrange(1,18) == 1
+if is_cursed:
+  race_name = "Cursed " + race_name
+
+
 first_letter = race_name[0:1].lower()
-your_class = random.choice(class_list)
 
 output_string = "You are "
 if first_letter in ['a', 'e', 'i', 'o', 'u']:
@@ -17,4 +22,8 @@ if first_letter in ['a', 'e', 'i', 'o', 'u']:
 else:
   output_string = output_string + 'a'
 
+your_class = random.choice(class_list)
+
+
 print (output_string, race_name, your_class)
+print ("base health:", race_health, "\nbase skills:", base_skills)
